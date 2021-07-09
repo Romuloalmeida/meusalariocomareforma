5da6e517e6cb30ef05232a7cf601f51115ace640
Romulo <r_sousa_17@hotmail.com>
2021-07-09 19:11:31 -0300

    Footer padding

--- source/src/App.js
+++ source/src/App.js
@@ -267,7 +267,7 @@ function App() {
         <p style={{ margin: 0, paddingBottom: "12px", color: "white" }}>Este projeto está em constante atualização e pode receber atualizações a qualquer momento.</p>
         <p style={{ margin: 0, paddingBottom: "12px", color: "white" }}>Sugestões? Por favor, me envie um e-mail em <a style={{ color: "white" }} href="mailto:romulosousa17@gmail.com">romulosousa17@gmail.com</a></p>
       </section>      
-      <footer style={{backgroundColor: "white"}}>
+      <footer style={{backgroundColor: "white",  paddingBottom: "100px"}}>
         <br/>
         <br/>
       <div className="fb-comments" data-href="https://meusalariocomareforma.com.br/" data-width="" data-numposts="1" data-colorScheme="dark"></div>  
